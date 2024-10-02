135312f21c3128780701188ec79c58e4715c91ed
Audrey Inglish <audrey.inglish@students.snow.edu>
2024-10-02 15:56:23 -0600

    started joke controller

---
+++ Dadabase/Dadabase/Controllers/JokeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dadabase.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class JokeController() : ControllerBase
+{
+
+
+    [HttpGet]
+    [EndpointName("GetJokeById")]
+    [EndpointSummary("Retrieves a joke from the dadabase, based on the requested ID")]
+    public string GetJokeById()
+    {
+        throw new NotImplementedException();
+    }
+    [HttpGet]
+    [EndpointName("GetRandomJoke")]
+    [EndpointSummary("Retrieves a random joke from the dadabase")]
+    public string GetRandomJoke()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class JokeDto
+{
+    public int Id { get; set; }
+    public  string JokeName { get; set; }
+    public  string JokeText { get; set; }   
+}
